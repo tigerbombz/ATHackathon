20b74e0dfe4a4c4b90005b95509371059715c7cb
Cliff Wright <clifford@cliffordwright.com>
2016-04-30 16:56:38 -0700

    integrate angular into rails app

--- htReportApp/app/assets/javascripts/app.js
+++ htReportApp/app/assets/javascripts/app.js
@@ -4,8 +4,24 @@ angular.module('sampleApp', ['ui.router','templates'])
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl:'_test.html',
+      templateUrl:'_homepage.html',
       controller: 'sampleCtrl'
+    })
+    .state('location', {
+      url: '/location',
+      templateUrl: '_location.html'
+    })
+    .state('minor', {
+      url: '/minor',
+      templateUrl: '_minor.html'
+    })
+    .state('photo', {
+      url: '/photo',
+      templateUrl: '_photo.html'
+    })
+    .state('thankyou', {
+      url: '/thankyou',
+      templateUrl: '_thankyou.html'
     })
 
   $urlRouterProvider.otherwise('/')
